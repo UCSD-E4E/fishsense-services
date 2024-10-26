ee0ab374a21e31e7676a900da5c6429fcd5f0cc1
Kyle Tran <trankyle2002@yahoo.com>
2024-10-25 19:59:26 -0700

    test

--- fishsense_services/routes/usr/login.py
+++ fishsense_services/routes/usr/login.py
@@ -1,9 +1,8 @@
 
 from os import abort
 from typing import Dict
-from urllib import request
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from google.auth.transport import requests
 from google.oauth2 import id_token
 
@@ -13,12 +12,12 @@ CLIENT_ID = "585544089882-2e8mni8kmbs39kekip1k6d09q5gjmqvv.apps.googleuserconten
 login_router = APIRouter()
 
 
-
+# possible routes: reset passwrod, need to check if there is account associated with it
 
 @login_router.post("/api/login")
 def verify_token():
     try:
-        token: Dict[str, str] = request.json
+        token: Dict[str, str] = Request.json
         idinfo = id_token.verify_oauth2_token(
             token["credential"], requests.Request(), CLIENT_ID
         )
