cfbb021dd1e5811193409c49aef7c49e9b5db851
Kyle Tran <trankyle2002@yahoo.com>
2024-10-31 12:17:49 -0700

    temp

---
+++ fishsense_services/routes/img/img.py
@@ -0,0 +1,65 @@
+from fastapi import APIRouter, Request, HTTPException
+from pydantic import BaseModel
+from fishsense_database.database import Database 
+from fishsense_database.image import upload_img, get_all_user_imgs, get_img
+
+img_router = APIRouter()
+
+# @img_router.get("/gallery/{username}")
+# async def get_all_imgs(username: str, email: str | None = None):
+    
+#     try:
+#         imgs = get_all_user_imgs(username, email)
+#         return imgs
+    
+#     except ValueError as e:
+#         raise HTTPException(status_code=404, detail="User does not exist")
+
+@img_router.post("/upload", status_code=200)
+async def upload_img():
+    
+    try:
+        data = await Request.json()
+        username = data["username"]
+        img_name = data["img_name"]
+        file_type = data["file_type"]
+        img = data["img"]
+        # TODO add other parameters
+        
+        upload_img(username, img_name, file_type, img)
+        
+        return 
+    
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail="User does not exist")
+    
+    
+@img_router.get("/img/{username}/{img_id}", status_code=200)
+async def get_img(username: str, img_id: str):
+    
+    try:
+        img = get_img(username, img_id)
+        return img
+    
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=e.args)
+    
+    
+@img_router.delete("/img/{username}/{img_name}", status_code=200)
+async def delete_img(username: str, img_name: str):
+    
+    try:
+        delete_img(username, img_name)
+        return
+    
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail="User does not exist")
+    
+    except FileNotFoundError as e:
+        raise HTTPException(status_code=404, detail="Image does not exist")
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal server error")
+    
+        
+    
