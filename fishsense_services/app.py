8fe9ef3ef1b9e105e18a497cabc5b5358384565e
Kyle Tran <trankyle2002@yahoo.com>
2024-11-12 13:12:27 -0800

    deleting old stuff

--- fishsense_services/app.py
+++ fishsense_services/app.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from fishsense_services.routes.usr.login_routes import login_router 
 from fishsense_services.routes.usr.user_routes import usr_router
-from fishsense_services.routes.processing.img_routes import img_router
 
 
 def create_app():
@@ -9,7 +8,6 @@ def create_app():
     
     app.include_router(login_router, prefix="/login")
     app.include_router(usr_router, prefix="/usr")
-    app.include_router(img_router, prefix="/img")
 
     return app
 
--- fishsense_services/routes/usr/user_routes.py
+++ fishsense_services/routes/usr/user_routes.py
@@ -1,9 +1,10 @@
-from fishsense_database.database import Database
+from fishsense_database.database import HTTP_CODES, Database
 from create_db import database
 
 import datetime
 from fastapi import APIRouter, Request, HTTPException
 from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 
 usr_router = APIRouter()
 
@@ -20,7 +21,7 @@ async def create_usr_route(request: Request):
 
             data["created_utc"] = time_diff
             data["last_login_utc"] = time_diff # TODO fix time stamp 
-            success = db.exec_script("fishsense-database/scripts/insert_scripts/insert_user.sql", data) 
+            success = db.exec_script("fishsense-database/scripts/insert_scripts/insert_user.sql", HTTP_CODES.POST, data) 
 
         if not success:
             raise ValueError("User with this username or email already exists.")
@@ -40,46 +41,57 @@ async def create_usr_route(request: Request):
         
    
     
-# @usr_router.get("/user={username}", status_code=200)
-# async def get_usr_route(username: str):
+@usr_router.get("/user={username}", status_code=200)
+async def get_usr_route(username: str):
 
-#     try:
-#         if not username:
-#             raise ValueError("Must provide a username.")
-#         user = get_user(username)
+    try:
+        if not username:
+            raise ValueError("Must provide a username.")
         
-#         if not user:
-#             raise ValueError("User does not exist.")
+        data = {"username": username}
         
-#         return JSONResponse(
-#             status_code=200,
-#             content={"user details": jsonable_encoder(user)}
-#         )
+        success = False
+        with database as db:
+            success = db.exec_script("fishsense-database/scripts/get_scripts/get_users_scripts/get_user_by_username.sql", HTTP_CODES.GET.value, data) 
+        
+        if not success:
+            raise ValueError("Error executing script")
+        
+        
+        return JSONResponse(
+            status_code=200,
+            content={"user details": jsonable_encoder(success)}
+        )
     
-#     except ValueError as e:
+    except ValueError as e:
         
-#         return JSONResponse(
-#             status_code=400,
-#             content={"detail": str(e)}  # Send the error message as string
-#         )
+        return JSONResponse(
+            status_code=400,
+            content={"detail": str(e)}  # Send the error message as string
+        )
     
-# @usr_router.get("/", status_code=200)
-# async def all_usrs_route():
+@usr_router.get("/", status_code=200)
+async def all_usrs_route():
 
-#     try:
-#         users = get_all_users()
+    try:
+        success = False
+        with database as db:
+            success = db.exec_script("fishsense-database/scripts/get_scripts/get_users_scripts/get_all_users.sql", http_code=HTTP_CODES.GET.value) 
         
-#         return JSONResponse(
-#             status_code=200,
-#             content={"users": jsonable_encoder(users)}
-#         )
+        if not success:
+            raise ValueError("Error executing script")
+        
+        return JSONResponse(
+            status_code=200,
+            content={"users": jsonable_encoder(success)}
+        )
     
-#     except ValueError as e:
+    except ValueError as e:
         
-#         return JSONResponse(
-#             status_code=400,
-#             content={"detail": str(e)}  # Send the error message as string
-#         )
+        return JSONResponse(
+            status_code=400,
+            content={"detail": str(e)}  # Send the error message as string
+        )
 
 # @usr_router.put("/update", status_code=200)
 # async def update_usr_route(request: Request):
